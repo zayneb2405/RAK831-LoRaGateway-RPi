fcdfa5c0f747cd4962e69b75d9269cd4b75c4cf7
Robert Lie <robertlie@users.noreply.github.com>
2019-02-22 09:17:24 +0100

    Set back reference to The Things Network Gateway

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR=$(pwd)
 VERSION="master"
 if [[ $1 != "" ]]; then VERSION=$1; fi
 
-echo "The RAK831 Gateway installer"
+echo "The Things Network Gateway installer"
 echo "Version: $VERSION"
 
 # Request gateway configuration data
